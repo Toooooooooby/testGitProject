35e81fa1d86a0192da905dc323ae2fdfe1ea08e1
Toooooooooby <xinyu_sun@yeah.net>
2023-05-14 00:58:07 +0800

    第一次修改

--- src/main/java/com/sxy/git/test1.java
+++ src/main/java/com/sxy/git/test1.java
@@ -2,6 +2,6 @@ package com.sxy.git;
 
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World！！！");
     }
 }
