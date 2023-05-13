8a1e18efd8a3d4b239dd8803e537ce1fb5786000
Toooooooooby <111356667+Toooooooooby@users.noreply.github.com>
2023-05-14 01:06:42 +0800

    Update test1.java
    第二次修改

--- src/main/java/com/sxy/git/test1.java
+++ src/main/java/com/sxy/git/test1.java
@@ -2,6 +2,7 @@ package com.sxy.git;
 
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("Hello World！！！");
+//         System.out.println("Hello World！！！");
+        System.out.println("哈哈哈哈哈");
     }
 }
